8bd1eabad37f0917c2db0904780eb42e2e96209a
dearonger <wuzhihan_1@163.com>
2018-07-16 21:29:17 +0800

    change number's value

---
+++ changeNumber.js
@@ -0,0 +1,15 @@
+//method1
+// var num1=10,num2=20;
+// console.log(num1);
+// var temp;
+// temp=num1;
+// num1=mun2;
+// num2=temp;
+// console.log(num1);
+
+//method2
+var num1=10,num2=20;
+num1=num1+num2;
+num2=num1-num2;
+num1=num1-num2;
+console.log(num1,num2);
