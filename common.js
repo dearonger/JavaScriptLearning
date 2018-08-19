6352142b07e69128f2c769f43ed1a40be85b7992
dearonger <wuzhihan_1@163.com>
2018-08-19 15:48:05 +0800

    WebAPI

--- common.js
+++ common.js
@@ -5,3 +5,28 @@
 function my$(id) {
   return document.getElementById(id);
 }
+
+//兼容代码
+function getInnerText(element) {
+  if (typeof element.innerText == "undefined") {
+    return element.textContent;
+  } else return element.innerText;
+}
+
+function setInnerText(element, text) {
+  if (typeof element.innerText == "undefined") {
+    element.textContent = text;
+  } else element.innerText = text;
+}
+
+//为任意元素.绑定任意的事件, 任意的元素,事件的类型,事件处理函数
+function addEventListener(element, type, fn) {
+  //判断浏览器是否支持这个方法
+  if (element.addEventListener) {
+    element.addEventListener(type, fn, false);
+  } else if (element.attachEvent) {
+    element.attachEvent("on" + type, fn);
+  } else {
+    element["on" + type] = fn;
+  }
+}
