5758e771192a9ca92b2ae4aae425cf2e40644346
dearonger <wuzhihan_1@163.com>
2018-08-20 19:44:08 +0800

    protoType

--- common.js
+++ common.js
@@ -30,3 +30,15 @@ function addEventListener(element, type, fn) {
     element["on" + type] = fn;
   }
 }
+
+//解绑事件的兼容
+//为任意的一个元素,解绑对应的事件
+function removeEventListener(element, type, fnName) {
+  if (element.removeEventListener) {
+    element.removeEventListener(type, fnName, false);
+  } else if (element.detachEvent) {
+    element.detachEvent("on" + type, fnName);
+  } else {
+    element["on" + type] = null;
+  }
+}
